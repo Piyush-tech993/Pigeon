c10f37ad0e5dd92c5e0eebd3bc498a67aa11b5be
SONI <sonipiyush993@gmail.com>
2021-05-01 15:30:15 +0530

    Add Files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pigeon - Fly High To Sky</title>
+    <link rel="shortcut icon" href="logo/Pigeon Logo.svg" type="image/x-icon">
+    <!-- Navbar CSS -->
+    <link rel="stylesheet" href="style/navbar.css">
+</head>
+<body>
+    <?php include "navbar.php"; ?>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="navbar__logo">
+        <img src="logo/Pigeon Logo.svg" alt="logo" class="logo">
+        <h2>Pigeon</h2>
+    </div>
+    <div class="sign">
+        <button class="sign__btn sign__in">Sign In</button>
+        <button class="sign__btn sign__up">Sign Up</button>
+    </div>
+</nav>
