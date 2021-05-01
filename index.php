3d162abae9ec5f62abd3e84085dc1392fcab0005
Piyush Soni <62609439+Piyush-tech993@users.noreply.github.com>
2021-05-01 19:12:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,17 @@
     <link rel="stylesheet" href="style/navbar.css">
 </head>
 <body>
-    <?php include "navbar.php"; ?>
+    <!-- Navbar Section-->
+    <nav class="navbar">
+    <div class="navbar__logo">
+        <img src="logo/Pigeon Logo.svg" alt="logo" class="logo">
+        <h2>Pigeon</h2>
+    </div>
+    <div class="sign">
+        <button class="sign__btn sign__in">Sign In</button>
+        <button class="sign__btn sign__up">Sign Up</button>
+    </div>
+    </nav>
+    <!-- !Navbar Section -->
 </body>
 </html>
